17415903bb532c41de035fee692247923c17ec99
Zhi Li <zli@babson.edu>
2017-11-14 11:57:37 -0500

    for 2017

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+
 from calculator import quadratic
 
 app = Flask(__name__)
 
-
 app.config['DEBUG'] = True
+
+app.secret_key = "Some secret string here"
 
 
 @app.route('/')
@@ -20,17 +22,21 @@ def hello(name=None):
     return render_template('hello.html', name=name)
 
 
-@app.route('/calculator/', methods=['GET', 'POST'])
+@app.route('/calc/', methods=['GET', 'POST'])
 def calculate():
     if request.method == 'POST':
         a = float(request.form['a'])
         b = float(request.form['b'])
         c = float(request.form['c'])
         root_1, root_2 = quadratic(a, b, c)
+
         if root_1:
             return render_template('calculator_result.html', a=a, b=b, c=c,
                                    root_1=root_1, root_2=root_2)
-    return render_template('calculator_form.html')
+        else:
+            return render_template('calculator_form.html', error=True)
+    return render_template('calculator_form.html', error=None)
+
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080)
+    app.run()
--- calculator.py
+++ calculator.py
@@ -2,11 +2,11 @@ import math
 
 
 def quadratic(a, b, c):
-    discriminant = b**2 - 4 * a * c  # calculate the discriminant
+    discriminant = b ** 2 - 4 * a * c  # calculate the discriminant
 
     if discriminant >= 0:  # equation has solutions
-        x_1 = ((-b + math.sqrt(discriminant)) / 2 * a)
-        x_2 = ((-b - math.sqrt(discriminant)) / 2 * a)
+        x_1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        x_2 = (-b - math.sqrt(discriminant)) / (2 * a)
         return x_1, x_2
 
     else:
@@ -19,6 +19,7 @@ def main():
     b = float(input('please enter a number:'))
     c = float(input('please enter a number:'))
     print('Results are:', quadratic(a, b, c))
+
 
 if __name__ == '__main__':
     main()
